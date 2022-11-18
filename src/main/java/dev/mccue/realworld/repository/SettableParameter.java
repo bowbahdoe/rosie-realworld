6d50ed95bde79b14642ea4e38f36adef1bba2ec9
Ethan McCue <ethan@mccue.dev>
2022-11-18 10:51:09 -0500

    no repository package

--- src/main/java/dev/mccue/realworld/repository/SettableParameter.java
+++
@@ -1,8 +0,0 @@
-package dev.mccue.realworld.repository;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface SettableParameter {
-    void setParameter(PreparedStatement statement, int i) throws SQLException;
-}
